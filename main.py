3de3e438729eb07c1f4140bcb7e011d626c56f24
Armand Packham-McGuiness <armand.packham@gmail.com>
2024-07-25 15:00:25 +1000

    More command support for scaling

--- main.py
+++ main.py
@@ -2,10 +2,6 @@ import turtle
 import inspect
 from tkinter import *
 
-
-
-
-
 class Camera:
     def __init__(self, x, y, objects, turtle, scale=1):
         self.x = x
@@ -21,18 +17,26 @@ class Camera:
         new_args = (args, kwargs)
         self.objects.append((command, new_args, (original_x, original_y)))
 
-    def draw(self, x, y,scale=1):
+    def draw(self, x, y, scale=1):
         self.turtle.reset()
         self.turtle.hideturtle()
         self.turtle.clear()
         self.turtle.penup()
-        self.turtle.teleport((-x) * scale,(-y) * scale)
+        self.turtle.goto((-x) * scale, (-y) * scale)
         self.turtle.showturtle()
         for command, (args, kwargs), (original_x, original_y) in self.objects:
             new_args = list(args)
             new_kwargs = dict(kwargs)
             sig = inspect.signature(command)
             params = sig.parameters
+
+            if command.__name__ in ['forward', 'fd', 'back', 'backward', 'bk', 'circle']:
+                if len(new_args) > 0:
+                    new_args[0] *= scale
+                if 'distance' in new_kwargs:
+                    new_kwargs['distance'] *= scale
+                if 'radius' in new_kwargs:
+                    new_kwargs['radius'] *= scale
 
             if 'x' in params and original_x is not None:
                 if 'x' in new_kwargs:
@@ -51,7 +55,6 @@ class Camera:
                         new_args[index] = (original_y - y) * scale
 
             command(*new_args, **new_kwargs)
-
 
 # Set up the screen
 screen = turtle.Screen()
@@ -82,19 +85,17 @@ class MouseEventHandler:
             x -= x_diff
             y -= y_diff
             self.canvas.draw(x, y, scale)
+
     def on_scroll(self, event):
         global scale, redraw
         
-        self.min_redraw = 5
+        self.min_redraw = 10
         redraw += abs(event.delta)
         scale -= (event.delta / 5)
         scale = max(0.1, scale)
         if self.min_redraw < redraw:
             redraw = 0
             c.draw(x, y, scale)
-
-
-
 
 c = Camera(x, y, objects, pen)
 c.add(pen.speed, 0)
@@ -103,16 +104,19 @@ c.add(pen.goto, 20, 20)
 c.add(pen.goto, 0, 40)
 c.add(pen.goto, -20, 20)
 c.add(pen.goto, 0, 0)
+c.add(pen.forward, 50)
+c.add(pen.back,50)
+c.add(pen.circle, 30)
 c.draw(x, y, scale)
+
+
 
 # Set up the mouse event handler
 mouse_handler = MouseEventHandler(c, screen)
 screen.onscreenclick(mouse_handler.on_mouse_down, 1)
 canvas = screen.getcanvas()
 canvas.bind("<ButtonRelease-1>", mouse_handler.on_mouse_up)
-
 canvas.bind_all("<MouseWheel>", mouse_handler.on_scroll)
-
 
 # Keep the window open and responsive
 screen.mainloop()
